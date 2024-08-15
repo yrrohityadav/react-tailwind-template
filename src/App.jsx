68c642c3535b894569f4902d5d28b67b31d3b5c8
yrrohityadav <yrrohit2004@gmail.com>
2024-08-15 21:50:04 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-black text-white h-screen w-screen flex items-center justify-center">
+      rohit
+    </div>
+    </>
+  )
+}
+
+export default App
